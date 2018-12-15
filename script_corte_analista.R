86da015e53ae4753add8cd6862d311fe683069d0
Daniela Rim <rim.dan96@gmail.com>
2018-12-15 12:04:27 -0300

    cambios de dumps/step
    arreglos menores a la funcion de corte y otros agregados

--- script_corte_analista.R
+++ script_corte_analista.R
@@ -4,6 +4,8 @@ plot_end <- function(vel, ff){
   df <- as.data.frame(filter(df, velocity == vel & fill.factor == ff))
   c <- as.numeric((abs(max(df[["TotEng"]])) + abs(min(df[["TotEng"]])))/2)
   df1 <- filter(df, TotEng <= c)
+  ###ojo, no siempre los dumps van a ser cada 1000 steps####
+  df1 <- as.data.frame(filter(df1, Step %% 10000 == 0 ))
   x <- as.vector(df1[["TotEng"]])
   y <- as.vector(lag(df1[["TotEng"]]))
   z <- as.data.frame(x-y)
@@ -11,12 +13,34 @@ plot_end <- function(vel, ff){
   df1 <- bind_cols(df1, z)
   z1 <- filter(df1, z == 0)
   min_step <- as.numeric(min(z1[4]))
-  return(paste("el step de corte sugerido es", min_step))
+  print(paste("el step de corte sugerido es", min_step))
   p1 <- ggplot(df, aes(Step,TotEng)) +
     geom_line() +
     xlab("Step") +
     ylab("TotEng")
   p1 <- p1 + geom_vline(xintercept = as.numeric(min_step), color = "red", size =1)
   p1 <- p1 + ggtitle(as.character(paste("velocity ", vel, " & fill factor ", ff)))
-    return(p1)  
-  }
+  p1 
+}
+
+setwd("/home/daniela/simulaciones_daniela/")
+dirs <- list.files(path="./", full.names = TRUE, pattern = "granular_*")
+
+###OJO: tamaño small###
+fill.factor <- as.numeric(ff)
+velocity  <- as.numeric(vel)
+
+directory <- paste(getwd(),"/granular_small_",fill.factor,"_",velocity,sep ="")
+print("DIRECTORY:")
+print(directory)
+setwd(directory)
+
+
+lo <- as.character(paste("output.",min_step,".gz", sep = ""))
+last_output <- as.data.frame(read.table(lo, header= FALSE, sep=' ', strip.white = TRUE, skip = 9, skipNul = TRUE,
+                                        col.names=c("id", "type", 
+                                                    "x", "y", "z",
+                                                    "vx", "vy", "vz",
+                                                    "omegax", "omegay", "omegaz", "NA")))
+last_output <- select(last_output,-NA.)
+
